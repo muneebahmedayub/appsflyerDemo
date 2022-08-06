cac2608446b6ccb7ad6345ae82f30151abee8d25
Muneeb Ahmed <65030135+muneebahmedayub@users.noreply.github.com>
2022-08-06 18:58:33 +0500

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import appsFlyer from 'react-native-appsflyer';
+
+appsFlyer.initSdk(
+  {
+    devKey: 'qyqrPbidC6srkE7MduvbmS',
+    isDebug: false,
+  },
+  (result) => {
+    console.log({ result });
+  },
+  (error) => {
+    console.error(error);
+  }
+);
 
 export default function App() {
   return (
